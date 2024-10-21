1cc201e58331352c5ef0f4a145703a99bbbb80eb
Santiago Palacio <spalacioc05@gmail.com>
2024-10-20 21:15:15 -0500

    Creación del proyecto

---
+++ Celebrity-Heights/src/main/java/com/celebrity/heights/CelebrityHeights.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.celebrity.heights;
+
+/**
+ *
+ * @author spala
+ */
+public class CelebrityHeights {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
